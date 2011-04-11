96bf90d0b6a6992c28b4853ae70d6568afb05956
zTrix <i@ztrix.me>
2011-04-11 12:07:05 +0800

    can login/logout now

--- static/public.js
+++ static/public.js
@@ -8,15 +8,25 @@ ctrl.start=function(){
 
 ctrl.setupClick=function(){
 	jQuery('#submit').click(function(){
-		var s='({uid:"'+jQuery('#uid').val()+'",email:"'+jQuery('#email').val()+'",password:"'+jQuery('#password').val()+'"})';
-		T['register'](eval(s)).success(function(){
-			for(var i in arguments){
-				alert(i);
+		var u=jQuery('#uid').val(),m=jQuery('#email').val(),p=jQuery('#password').val();
+		var d=T['register'](kv2obj('uid',u,'email',m,'password',p));
+		d.success(function(resp,state,o){
+			if(resp['success']==1){
+				T.login(kv2obj('uid',u,'password',p)).success(function(){
+					window.location.reload(true);
+				});
+			}else if(resp['error']<0){
+				console.log('firetruck, error !');
 			}
+			console.log(arguments);
+		});
+		d.error(function(){
+		
 		});
 	});
 };
 
+// start the world when DOM ready
 jQuery(function(){
 	ctrl.start();
 });
