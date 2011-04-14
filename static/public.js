d1f377ec588529e06d258c2e94af742fe3aba5c9
zTrix <i@ztrix.me>
2011-04-14 14:41:10 +0800

    add login/reg animation

--- static/public.js
+++ static/public.js
@@ -18,33 +18,72 @@ K.PUBLIC={
 		c.setupClick();
 	};
 	c.setupClick=function(){
-		jQuery('button#submit').click(function(){
+		var o=jQuery('button#regbtn');
+		o.click(function(){
 			var u=jQuery('#uid').val(),p=jQuery('#password').val(),m=jQuery('#email').val();
-			if(!c.checkRegister(u,p,m)){
-				return;
-			}
-			T.register({uid:u,email:m,password:p}).success(function(resp,state,o){
-				if(resp['success']==1){
-					c.login(u,p);
-				}else if((typeof resp.error)!='undefined'){
-					if(resp.error==-4){
-						c.showError(K.PUBLIC.INVALID_UID);
-					}else{
-						console.log('server resp received,but,firetruck,error !');
-						console.log(resp);
+			if(o.attr('mark')=="reg"){
+				if(!c.checkRegister(u,p,m))return;
+				T.register({uid:u,email:m,password:p}).success(function(resp,state,o){
+					if(resp['success']==1){
+						c.showError("");
+						c.login(u,p);
+					}else if((typeof resp.error)!='undefined'){
+						if(resp.error==-4){
+							c.showError(K.PUBLIC.INVALID_UID);
+						}else{
+							console.log('server resp received,but,firetruck,error !');
+							console.log(resp);
+						}
 					}
-				}
-			}).error(function(){
-				console.log('server too far away to reach, I believe it\'s 500, server internel error');
-				c.showError(K.PUBLIC.SERVER_ERR);
-			});
+				}).error(function(){
+					console.log('server too far away to reach, I believe it\'s 500, server internel error');
+					c.showError(K.PUBLIC.SERVER_ERR);
+				});
+			}else{
+				if(!c.checkLogin(u,p))return;
+				c.showError("");
+				c.login(u,p);
+			}
 		});
-		jQuery('button#login').click(function(){
-			var u=jQuery('#uid').val(),p=jQuery('#password').val();
-			c.login(u,p);
+		$('a#toggle').click(function(){
+			if(o.attr('mark')=='login'){
+				o.attr('mark','reg');
+				o.children().text('注册');
+				c.toggleTip('已有帐号，直接登录');
+				$('a#forgetPass').animate({opacity:0},200);
+				$('button#regbtn').animate({
+					left:'+=167'
+				},600,function(){
+					$('div#emailDiv').animate({opacity:1},200);
+				});
+			}else{
+				o.attr('mark','login');
+				o.children().text('登录');
+				c.toggleTip('没有帐号？注册一个吧');
+				$('a#forgetPass').animate({opacity:1},800);
+				$('div#emailDiv').animate({opacity:0},200);
+				$('button#regbtn').animate({
+					left:'-=167'
+				});
+			}
+		});
+	};
+	c.toggleTip=function(s){
+		if(!s)return;
+		$('a#toggle').animate({opacity:0},200,function(){
+			$('a#toggle').text(s);
+			$('a#toggle').animate({opacity:1},200);
 		});
 	};
 	c.checkRegister=function(u,p,m){
+		if(!this.checkLogin(u,p))return false;
+		if(!/.+@.+\..+/i.test(m)){
+			this.showError(K.PUBLIC.NOT_EMAIL);
+			return false;
+		}
+		return true;
+	};
+	c.checkLogin=function(u,p){
 		if(u.length<2){
 			this.showError(K.PUBLIC.UID_TOO_SHORT);
 			return false;
@@ -53,12 +92,8 @@ K.PUBLIC={
 			this.showError(K.PUBLIC.PASS_TOO_SHORT);
 			return false;
 		}
-		if(!/.+@.+\..+/i.test(m)){
-			this.showError(K.PUBLIC.NOT_EMAIL);
-			return false;
-		}
 		return true;
-	};
+	},
 	c.showError=function(s){
 		if(U&&U.PAGE.statusDiv){
 			U.PAGE.statusDiv.show(s);
