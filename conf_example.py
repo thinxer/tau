a6f71fc4592c7573657fcc7d4a45ea6478a1ce7e
Jianfei Wang <me@thinxer.com>
2011-04-18 20:46:23 +0800

    update conf_example.conf

--- conf_example.py
+++ conf_example.py
@@ -5,8 +5,15 @@ version = '1'
 reserved_names = set(["www", "admin", "setting", "settings", \
         "login", "logout", "api"])
 
-media_uri_prefix = '/static/photos/'
+photo_uri_prefix = '/static/photos/'
+default_photo_uri = '/static/photos/_.png'
 import os.path
-media_file_path = os.path.join(os.path.dirname(__file__), "static/photos")
+photo_file_path = os.path.join(os.path.dirname(__file__), "static/photos")
+photo_size = 60
+photo_type = 'JPEG'
+photo_ext = '.jpg'
 
-debug = False
+# max stream item number in a query
+stream_item_max = 50
+
+debug = True
