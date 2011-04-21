98f644c4284ee24bc3ec3f981a98e7ccbb6448ec
zTrix <i@ztrix.me>
2011-04-21 12:23:54 +0800

    beautify home page, I believe thinxer won't like it~~

--- static/javascript/view/home.js
+++ static/javascript/view/home.js
@@ -5,35 +5,52 @@
     R.path('home',C.PAGE.goDefault);
 	var c=C[name]={};
     var u=U[name]={};
+	var markup=' <li class="post rr10"> <div style="position:absolute;left:0px;top:0px;"> <span class="larrow"></span> <a href="" class="avatar rr6" style="background:url(/static/photo/_.png)"></a> </div> <div> <a href="#" style="color:green;">${uid}</a>: </div> <div> ${content} </div> </li> ';
+	jQuery.template('post',markup);
+
 
 	c.start=function(){
 		c.setupClick();
+		c.updateCurUser();
 		c.updateTimeline();
 	};
 	c.setupClick=function(){
-		jQuery('button#logoutBtn').click(function(){
+		jQuery('#logout').click(function(){
 			T.logout().then(function(){
 				if(window.location.hash!="#public")R.path('public');
 				else R.reload();
 			});
 		});
-		jQuery('a#pubBtn').click(function(){
-			var o=$('textarea#publisher');
-			T.publish({content:o.val()}).success(function(){
+		var publish=function(){
+			var o=$('textarea#publisher'),v=o.val();
+			if(!v.length){
+				// show error ?
+				return;
+			}
+			T.publish({content:v}).success(function(){
 				o.text('');
 				U.PAGE.statusDiv.show('发布成功！');
 			}).error(function(){
 			});
+		};
+		jQuery('a#pubBtn').click(publish);
+		jQuery('#publisher').keypress(function(e){
+			if(e.which==10){
+				publish();
+			}
 		});
 	};
 	c.updateTimeline=function(){
 		var r=T.stream({}).success(function(r,s,o){
-			var t='';
-			for(var i in r.items){
-				t+='<li class="post rr10"><div>'+r.items[i].content+'</div></li>';
-			}
-			console.log(t);
-			$('ol#posts').append(t);
+			console.log(r.items);
+			jQuery.tmpl('post',r.items).appendTo('ol#posts');
+		});
+	};
+	c.updateCurUser=function(){
+		T.current_user({}).success(function(r){
+			
+		}).error(function(){
+		
 		});
 	};
 
