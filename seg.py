25f36750292378efa02f7fb2625ec1c3427ba607
Jianfei Wang <me@thinxer.com>
2011-04-27 09:20:44 +0800

    introduce pymmseg-cpp as a text segmentor

---
+++ seg.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+from pymmseg import mmseg
+mmseg.dict_load_defaults()
+
+def segment(text):
+    '''
+    text should be either utf8 or unicode
+    return a list of words in unicode
+    '''
+
+    if isinstance(text, unicode):
+        text = text.encode('utf8')
+    alg = mmseg.Algorithm(text)
+    # print '%s [%d..%d]' % (tok.text, tok.start, tok.end) for tok in alg
+    return [tok.text.decode('utf8') for tok in alg]
+
+def main():
+    print segment('今天天气不错hello world!')
+
+if __name__ == '__main__':
+    main()
+
