33b4bcb155d5b664b0c878e80761beed5c810f1a
zTrix <i@ztrix.me>
2011-04-30 19:40:52 +0800

    update stream when scrolling to bottom in profile page

--- static/javascript/view/profile.js
+++ static/javascript/view/profile.js
@@ -6,6 +6,36 @@
     var c = C[name] = {}, u = U[name] = {};
 
     var cur_uid;
+    var updating = false;
+
+    var updateStream = function(){
+        if (updating) return;
+        updating = true;
+        
+        var p = {uid: cur_uid};
+        if ($('ol.timeline>li').last().attr('data-hasmore')) {
+            p.olderThan = +$('ol.timeline>li .timestamp').last().attr('data-timestamp');
+        }
+        T.stream(p).success(function(r) {
+            $(r.items).each(function(i, e) {
+                e['user'] = r.users[e.uid];
+            });
+            U.render('stream_item', r.items, {
+                getDate: C.HOME.getReadableDate,
+                getName: function(uid, name){   // TODO: use name or uid ? 
+                    if (name && name.length > 0) return name;
+                    return uid;
+                }
+            }).appendTo('ol.timeline').done(function(t){
+                if (r.has_more) {
+                    t.last().attr('data-hasmore', 'true');
+                } else {
+                    $(document).unbind('scroll');
+                }
+                updating = false;
+            });
+        });
+    };
 
     var getDataAndShow = function(){
         if (!cur_uid) {
@@ -14,23 +44,10 @@
             return;
         }
         T.userinfo({uid: cur_uid}).success(function(d){
-            U.render('profile', d, { }).fillTo('#main').done(function(){
-                T.stream({uid: cur_uid}).success(function(r){
-                    $(r.items).each(function(i, e){
-                        e['user'] = r.users[e.uid];
-                    });
-                    U.render('stream_item', r.items, {
-                       getDate: C.HOME.getReadableDate
-                    }).appendTo('ol.timeline').done(function(t){
-                        if (r.has_more) {
-                            t.last().attr('data-hasmore', 'true');
-                        }
-                    });
-                });
-            });
+            U.render('profile', d, { }).fillTo('#main').done(updateStream);
         });
     };
-    
+
     R.path('u', {
         enter: function(){
             U.PAGE.header.show();
@@ -44,10 +61,17 @@
                     // TODO: WTF ?
                 }
                 getDataAndShow();
+                $(document).scroll(function(){
+                    if ($(window).scrollTop() > $(document).height() - $(window).height() - 20) {
+                        if ($('ol.timeline>li').last().attr('data-hasmore')) {
+                            updateStream();
+                        }
+                    }
+                });
             }
         },
         leave: function(){
-
+            $(document).unbind('scroll');
         }
     });
 
