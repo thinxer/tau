d609c5b0a3a6e1ebe06aae53b5ed69969b24c559
Jianfei Wang <me@thinxer.com>
2011-05-04 14:42:04 +0800

    enhance gettext.py with the ability to extract _("hello %s", "world")

--- i18n/gettext.py
+++ i18n/gettext.py
@@ -3,10 +3,13 @@ import re
 import sys
 
 tmpl = re.compile(r'_\((.*?)\)')
+es = re.compile(r'(".*?")|(\'.*?\')')
 
 for filename in sys.argv:
     f = open(filename)
     for item in tmpl.finditer(f.read()):
         s = item.groups()[0]
+        c = es.search(s).groups()
+        s = c[0] or c[1]
         sys.stdout.write(s + '\n')
     f.close()
